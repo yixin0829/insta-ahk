68618dcc3c6e7ff3ab95a5e4dcd9415cf3ca2e27
yixin0829 <56566212+yixin0829@users.noreply.github.com>
2021-12-06 21:19:06 -0500

    Adam is born

---
+++ adam.py
@@ -0,0 +1,7 @@
+from ahk import AHK
+
+ahk = AHK()
+
+# ahk.mouse_move(x=100, y=100, blocking=True)  # Blocks until mouse finishes moving (the default)
+ahk.mouse_move(x=150, y=150, speed=10, blocking=True) # Moves the mouse to x, y taking 'speed' seconds to move
+print(ahk.mouse_position)  #  (150, 150)
